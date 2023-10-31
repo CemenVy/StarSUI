9bc126d161f09055b7a89968d451b8cc6b471fd6
Семен Выдрин <pw5hv7ngcv@privaterelay.appleid.com>
2023-10-31 10:40:03 +0400

    Initial Commit

---
+++ StarSUI/StarSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarSUIApp.swift
+//  StarSUI
+//
+//  Created by Семен Выдрин on 31.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct StarSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
