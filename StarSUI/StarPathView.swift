bd8a1a837fdb729220e27dd541092f6bb3e47fdb
Семен Выдрин <pw5hv7ngcv@privaterelay.appleid.com>
2023-10-31 15:26:28 +0400

    Work with UI

--- StarSUI/StarPathView.swift
+++ StarSUI/StarPathView.swift
@@ -18,26 +18,15 @@ struct StarPathView: View {
             
             let minimumSize = min(width,height)
             let nearLine = minimumSize * 0.1
-            let farLine = minimumSize - nearLine
             let middle = minimumSize / 2
             let quarter = middle / 2
             
-            let pointOne = minimumSize * 0.15
-            let pointTwo = minimumSize * 0.175
+            let outerStarPoint = minimumSize * 0.15
+            let innerStarPoint = minimumSize * 0.175
             
+            // Основная звезда
             Path { path in
-                path.addLines([
-                    CGPoint(x: middle, y: nearLine),
-                    CGPoint(x: middle + pointTwo, y: middle - pointOne),
-                    CGPoint(x: farLine, y: middle - nearLine),
-                    CGPoint(x: middle + quarter, y: middle + nearLine),
-                    CGPoint(x: middle + quarter, y: farLine),
-                    CGPoint(x: middle, y: middle + quarter),
-                    CGPoint(x: middle - quarter, y: farLine),
-                    CGPoint(x: middle - quarter, y: middle + nearLine),
-                    CGPoint(x: nearLine, y: middle - nearLine),
-                    CGPoint(x: middle - pointTwo, y: middle - pointOne)
-                ])
+                createStarPath(in: &path, with: width, and: height)
             }
             .fill(
                 RadialGradient(
@@ -48,53 +37,59 @@ struct StarPathView: View {
                 )
             )
             
-            Path { path in
-                path.move(to: CGPoint(x: middle + pointTwo, y: middle - pointOne))
-                path.addLine(to: CGPoint(x: middle + quarter, y: middle + nearLine))
-            }
-            .stroke(colorLine,
-            style: StrokeStyle(lineWidth: 4))
+            // Четыре линии от внутренних вершин
+            drawLine(at: CGPoint(x: middle + innerStarPoint, y: middle - outerStarPoint),
+                     to: CGPoint(x: middle + quarter, y: middle + nearLine))
+            drawLine(at: CGPoint(x: middle + innerStarPoint, y: middle - outerStarPoint),
+                     to: CGPoint(x: middle, y: middle + quarter))
+            drawLine(at: CGPoint(x: middle + innerStarPoint, y: middle - outerStarPoint),
+                     to: CGPoint(x: middle - quarter, y: middle + nearLine))
+            drawLine(at: CGPoint(x: middle + innerStarPoint, y: middle - outerStarPoint),
+                     to: CGPoint(x: middle - innerStarPoint, y: middle - outerStarPoint))
             
+            // Звезда для создания контура
             Path { path in
-                path.move(to: CGPoint(x: middle + pointTwo, y: middle - pointOne))
-                path.addLine(to: CGPoint(x: middle, y: middle + quarter))
+                createStarPath(in: &path, with: width, and: height)
             }
             .stroke(colorLine,
-            style: StrokeStyle(lineWidth: 4))
-
-            
-            Path { path in
-                path.move(to: CGPoint(x: middle + pointTwo, y: middle - pointOne))
-                path.addLine(to: CGPoint(x: middle - quarter, y: middle + nearLine))
-            }
-            .stroke(colorLine,
-            style: StrokeStyle(lineWidth: 4))
-            
-            Path { path in
-                path.move(to: CGPoint(x: middle + pointTwo, y: middle - pointOne))
-                path.addLine(to: CGPoint(x: middle - pointTwo, y: middle - pointOne))
-            }
-            .stroke(colorLine,
-            style: StrokeStyle(lineWidth: 4))
-            
-            Path { path in
-                path.addLines([
-                    CGPoint(x: middle, y: nearLine),
-                    CGPoint(x: middle + pointTwo, y: middle - pointOne),
-                    CGPoint(x: farLine, y: middle - nearLine),
-                    CGPoint(x: middle + quarter, y: middle + nearLine),
-                    CGPoint(x: middle + quarter, y: farLine),
-                    CGPoint(x: middle, y: middle + quarter),
-                    CGPoint(x: middle - quarter, y: farLine),
-                    CGPoint(x: middle - quarter, y: middle + nearLine),
-                    CGPoint(x: nearLine, y: middle - nearLine),
-                    CGPoint(x: middle - pointTwo, y: middle - pointOne)
-                ])
-                path.closeSubpath()
-            }
-            .stroke(colorLine,
-            style: StrokeStyle(lineWidth: 4))
+                    style: StrokeStyle(lineWidth: 4))
         }
+    }
+    
+    private func createStarPath(in path: inout Path, with width: CGFloat, and height: CGFloat) {
+        let width = width
+        let height = height
+        
+        let minimumSize = min(width,height)
+        let nearLine = minimumSize * 0.1
+        let farLine = minimumSize - nearLine
+        let middle = minimumSize / 2
+        let quarter = middle / 2
+        
+        let pointOne = minimumSize * 0.15
+        let pointTwo = minimumSize * 0.175
+        
+        path.addLines([
+            CGPoint(x: middle, y: nearLine),
+            CGPoint(x: middle + pointTwo, y: middle - pointOne),
+            CGPoint(x: farLine, y: middle - nearLine),
+            CGPoint(x: middle + quarter, y: middle + nearLine),
+            CGPoint(x: middle + quarter, y: farLine),
+            CGPoint(x: middle, y: middle + quarter),
+            CGPoint(x: middle - quarter, y: farLine),
+            CGPoint(x: middle - quarter, y: middle + nearLine),
+            CGPoint(x: nearLine, y: middle - nearLine),
+            CGPoint(x: middle - pointTwo, y: middle - pointOne)
+        ])
+        path.closeSubpath()
+    }
+    
+    private func drawLine(at start: CGPoint, to end: CGPoint) -> some View {
+        Path { path in
+            path.move(to: start)
+            path.addLine(to: end)
+        }
+        .stroke(colorLine, style: StrokeStyle(lineWidth: 4))
     }
     
 }
